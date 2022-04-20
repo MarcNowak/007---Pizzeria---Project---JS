2858d3ca654746d6a935e110371a9f11a9fa802a
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-04-20 11:51:34 +0200

    Add 8.7 TASK - picrure displaying - DONE

--- src/js/script.js
+++ src/js/script.js
@@ -100,6 +100,9 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      // referencja do pojedynczego elementu o selektorze zapisanym w select.menuProduct.imageWrapper
     }
 
     initAccordion() {                              /* tworzymy metodę initAccordion */
@@ -182,7 +185,10 @@
           // console.log(optionId, option);
 
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if (formData[paramId] && formData[paramId].includes(optionId)) {
+          // if (formData[paramId] && formData[paramId].includes(optionId)) {
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+
+          if (optionSelected){
 
             // check if the option is not default
             if (!option.default == true) {
@@ -196,6 +202,18 @@
 
               // reduce price variable
               price -= option.price;
+          }
+
+          // wyświetlanie obrazków 
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          // szukamy obrazka odpowiadającego konkretnej parze kategoria-opcja
+
+          if(optionImage) {
+            if(optionSelected) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
           }
         }
       }
