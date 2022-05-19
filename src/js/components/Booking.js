641990426454d863cfb83a28a2557b74448f5643
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-05-19 08:57:09 +0200

    Add 11.3 Pizzeria - DOM element update

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,4 @@
-import { select, settings, templates } from '../settings.js';
+import { classNames, select, settings, templates } from '../settings.js';
 import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
@@ -96,12 +96,124 @@ class Booking {
       .then(function ([bookings, eventsCurrent, eventsRepeat]) {
         /* ten zapis oznacza: potraktuj pierwszy argument jako tablicę
         i pierwszy element tej tablicy zapisz w zmiennej bookings */
-        console.log(bookings);
-        console.log(eventsCurrent);
-        console.log(eventsRepeat);
-      });
+        // console.log(bookings);
+        // console.log(eventsCurrent);
+        // console.log(eventsRepeat);
 
+        thisBooking.parseData(bookings, eventsCurrent, eventsRepeat);
+      });
   }
+
+  parseData(bookings, eventsCurrent, eventsRepeat) {
+    const thisBooking = this;
+
+    thisBooking.booked = {};
+
+    /* pętla iteruje przez wszystkie zwykłe bookingi */
+    for (let item of bookings) {
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
+    }
+
+    /* pętla iteruje przez wszystkie wydarzenia jednorazowe */
+    for (let item of eventsCurrent) {
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
+    }
+
+    const minDate = thisBooking.datePicker.minDate;
+    const maxDate = thisBooking.datePicker.maxDate;
+
+    /* pętla iteruje przez wszystkie wydarzenia cykliczne */
+    for (let item of eventsRepeat) {
+      if (item.repeat == 'daily') {
+        for (let loopDate = minDate; loopDate <= maxDate; loopDate = utils.addDays(loopDate, 1)){
+          thisBooking.makeBooked(utils.dateToStr(loopDate), item.hour, item.duration, item.table);
+
+        }
+      }
+    }
+
+    // console.log('thisBooking.booked: ', thisBooking.booked);
+
+    thisBooking.updateDOM();
+  }
+
+  makeBooked(date, hour, duration, table) {
+    const thisBooking = this;
+
+    /* sprawdzamy czy mamy już jakikolwiek wpis w thisBooking.booked
+    dla tej konkretnej daty */
+    if (typeof thisBooking.booked[date] == 'undefined') {
+      /* jeśli nie, to chcemy stworzyć pusty obiekt */
+      thisBooking.booked[date] = {};
+    }
+
+    /* konwertujemy godzinę na liczbę */
+    const startHour = utils.hourToNumber(hour);
+
+    for (let hourBlock = startHour; hourBlock < startHour + duration; hourBlock += 0.5) {
+      // console.log('loop: ', hourBlock);
+      /* sprawdzamy czy mamy już jakikolwiek wpis w thisBooking.booked
+dla tej konkretnej daty i godziny */
+      if (typeof thisBooking.booked[date][hourBlock] == 'undefined') {
+        /* jeśli nie, to chcemy stworzyć pustą tablicę */
+        thisBooking.booked[date][hourBlock] = [];
+      }
+
+      /* dla danej daty i godziny chcemy dodać stolik
+      do listy zajętych stolików, czyli thisBooking.book */
+      thisBooking.booked[date][hourBlock].push(table);
+    }
+  }
+
+  updateDOM(){
+    const thisBooking = this;
+
+    /* zapisujemy właściwości date oraz hour 
+    w oparciu o wartości widgetów datePicker i hourPicker
+    są to wartości wybrane aktualnie przez użytkownika */
+    thisBooking.date = thisBooking.datePicker.value;
+    thisBooking.hour - utils.hourToNumber(thisBooking.hourPicker.value);
+
+    let allAvailable = false;
+
+    if (
+      /* jeśli w obiekcie thisBooking.booked dla tej daty nie ma obiektu */
+      typeof thisBooking.booked[thisBooking.date] == 'undefined'
+      ||
+      /* lub dla tej daty i godziny nie istnieje tablica */
+      typeof thisBooking.booked[thisBooking.date][thisBooking.hour] == 'undefined'
+    ){
+      /* znaczy to, że wszystkie stoliki są dostępne
+      i zmieniamy wartość zmiennej na true */
+      allAvailable = true;
+    }
+
+    /* iterując przez wszystkie stoliki */
+    for (let table of thisBooking.dom.tables){
+      /* pobieramy ID aktualnego stolika */
+      let tableId = table.getAttribute(settings.booking.tableIdAttribute);
+      if(!isNaN(tableId)){
+        
+        /* konwertujemy tekst na liczbę */
+        tableId = parseInt(tableId);
+      }
+
+      /* if sprawdza czy nie wszystkie stoliki są dostępne
+      czy któryś stolik jest zajęty */
+      if(
+        !allAvailable
+        &&
+        /* czy tego dnia o danej godzinie jest zajęty stolik o danym ID */
+        thisBooking.booked[thisBooking.date][thisBooking.hour].includes(tableId)
+      ){
+        table.classList.add(classNames.booking.tableBooked);
+      } else {
+        table.classList.remove(classNames.booking.tableBooked);
+      }
+    }
+  }
+
+
 
   render(element) {
     const thisBooking = this;
@@ -127,6 +239,9 @@ class Booking {
     /* przygotowujemy referencje do obu inputów */
     thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
     thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
+
+    /* referncja do stolików widocznych na mapie */
+    thisBooking.dom.tables = thisBooking.dom.wrapper.querySelectorAll(select.booking.tables);
   }
 
   initWidgets() {
@@ -145,6 +260,10 @@ class Booking {
 
     thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
     thisBooking.dom.hourPicker.addEventListener('click', function () { });
+
+    thisBooking.dom.wrapper.addEventListener('updated', function(){
+      thisBooking.updateDOM();
+    });
   }
 }
 
